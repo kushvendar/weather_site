6d28ae358c45e16801ba24afb4ab709da7aae088
Kush <jangidkush1234@gmail.com>
2025-01-25 00:23:20 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import search from './images/search.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex flex-col items-center justify-center h-screen bg-blue-400'>
+      <div className='flex flex-center'>
+        <div><input type="text" placeholder='enter city name' /></div>
+        <div><img src={search} alt="search bar" /></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className=''>weather pic</div>
+      <div className=''>temp</div>
+      <div className=''> place</div>
+      <div className=''>humidity and wind speed</div>
+    </div>
   )
 }
 
