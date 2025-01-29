90015c7574537d864b131fc9bfe78b488d30d123
Kush <jangidkush1234@gmail.com>
2025-01-29 22:57:53 +0530

    added weather api

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
+const { default: axios } = require('axios')
 const express=require('express')
+const { env } = require('process')
 
 const app=express()
 
+app.get('/',(req,res)=>{
+   axios.get(env("url")) 
+})
 
 app.listen(3000,()=>{
     console.log(`server is running on port 3000`)
