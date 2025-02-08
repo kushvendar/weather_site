446eae84fb9b436077e7ea4a2b9cbc44d0cfad26
Kush <jangidkush1234@gmail.com>
2025-02-08 23:06:42 +0530

    frontend half done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,27 @@
 import './App.css'
-import clear from './images/clear.png'
-import cloud from './images/clouds.png'
-import drizzle from './images/drizzle.png'
-import humidity from './images/humidity.png'
-import mist from './images/mist.png'
-import rain from './images/rain.png'
+// import clear from './images/clear.png'
+// import cloud from './images/clouds.png'
+// import drizzle from './images/drizzle.png'
+// import humidity from './images/humidity.png'
+// import mist from './images/mist.png'
+// import rain from './images/rain.png'
 import search from './images/search.png'
-import snow from './images/snow'
-import clear from './images/clear.png'
+// import snow from './images/snow'
+// import clear from './images/clear.png'
 
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-blue-400'>
-      <div className='flex flex-center'>
-        <div><input type="text" placeholder='enter city name' /></div>
-        <div><img src={search} alt="search bar" /></div>
-      </div>
-      <div className=''><img src={cloud} alt="cloud" /></div>
-      <div>pm level </div>
+        <div className='bg-black flex flex-col '>
+          <div className= 'flex flex-col bg-gradient-to-br from-[#00feba] to-[#5b548a] h-[470px] max-w-90 m-[20px] p-[30px] rounded-3xl'>
+        <div className='flex flex-row'><input type="text" placeholder='enter city name' />
+        <img src={search} alt="search bar" /> 
+        </div> 
       <div className=''>temp</div>
       <div className=''> place</div>
       <div className=''>humidity and wind speed</div>
     </div>
+        </div>
   )
 }
 
